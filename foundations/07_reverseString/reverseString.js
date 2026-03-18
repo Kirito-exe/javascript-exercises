49a5b869c9a7ae35bd1479e0b2ac8321a4afe6ad
Kirito-exe <mannuprakash935@gmail.com>
2026-03-19 02:15:17 +0530

    did 6 and 7, 67

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse='';
+    if (!word) return "";
+    let i = word.length;
+    do {
+        reverse += word[i-1]
+        i--
+    } while (i>0)
+    return reverse;
 };
 
 // Do not edit below this line
