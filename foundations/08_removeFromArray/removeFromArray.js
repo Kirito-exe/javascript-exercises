4ec67beb5a232a3db77ef2ef67dcc3a899537916
Kirito-exe <mannuprakash935@gmail.com>
2026-03-19 04:31:09 +0530

    8 9 10 done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr,...args) {
+    function inclusion(num){
+        return !args.includes(num);
+    }
+    const newarry =arr.filter(inclusion);
+    return newarry;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
